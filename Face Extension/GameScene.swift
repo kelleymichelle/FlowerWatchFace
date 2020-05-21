ff33c0b3078fc79bcaedf82ebd88733cd16923b9
Kelley Chaplain <kmchaplain@gmail.com>
2020-05-21 17:00:09 -0500

    added label to add date to face

--- Face Extension/GameScene.swift
+++ Face Extension/GameScene.swift
@@ -13,19 +13,19 @@ class GameScene: SKScene {
     var minuteHand:SKSpriteNode = SKSpriteNode()
     var hourHand:SKSpriteNode = SKSpriteNode()
     var faceBackground:SKSpriteNode = SKSpriteNode()
-//    let faces = []
-    let one = SKTexture(image: #imageLiteral(resourceName: "1.png"))
-    let two = SKTexture(image: #imageLiteral(resourceName: "2.png"))
-    let three = SKTexture(image: #imageLiteral(resourceName: "3.png"))
-    let four = SKTexture(image: #imageLiteral(resourceName: "4.png"))
-    let five = SKTexture(image: #imageLiteral(resourceName: "5.png"))
-    let six = SKTexture(image: #imageLiteral(resourceName: "6.png"))
-    let seven = SKTexture(image: #imageLiteral(resourceName: "7.png"))
-    let eight = SKTexture(image: #imageLiteral(resourceName: "8.png"))
-    let nine = SKTexture(image: #imageLiteral(resourceName: "9.png"))
-    let ten = SKTexture(image: #imageLiteral(resourceName: "10.png"))
-    let eleven = SKTexture(image: #imageLiteral(resourceName: "12.png"))
-    let twelve = SKTexture(image: #imageLiteral(resourceName: "12.png"))
+
+//    let one = SKTexture(image: #imageLiteral(resourceName: "1.png"))
+//    let two = SKTexture(image: #imageLiteral(resourceName: "2.png"))
+//    let three = SKTexture(image: #imageLiteral(resourceName: "3.png"))
+//    let four = SKTexture(image: #imageLiteral(resourceName: "4.png"))
+//    let five = SKTexture(image: #imageLiteral(resourceName: "5.png"))
+//    let six = SKTexture(image: #imageLiteral(resourceName: "6.png"))
+//    let seven = SKTexture(image: #imageLiteral(resourceName: "7.png"))
+//    let eight = SKTexture(image: #imageLiteral(resourceName: "8.png"))
+//    let nine = SKTexture(image: #imageLiteral(resourceName: "9.png"))
+//    let ten = SKTexture(image: #imageLiteral(resourceName: "10.png"))
+//    let eleven = SKTexture(image: #imageLiteral(resourceName: "12.png"))
+//    let twelve = SKTexture(image: #imageLiteral(resourceName: "12.png"))
     
     let newTexture = SKTexture(imageNamed: "1")
     
@@ -47,7 +47,6 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     
-        faceBackground.texture = two
         let date = Date()
         let calendar = Calendar.current
         let hour = CGFloat(calendar.component(.hour, from: date))
